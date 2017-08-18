3f5133fdac2757d884906569f7e7dd152ed90e08
Olaf Japp <artanidos@gmail.com>
2017-08-18 16:05:46 +0200

    slidereditor removed

--- moduldialog.cpp
+++ moduldialog.cpp
@@ -30,11 +30,11 @@ ModulDialog::ModulDialog()
     QGridLayout *grid = new QGridLayout();
     FlatButton *b1 = new FlatButton(":/images/text_normal.png", ":/images/text_hover.png");
     FlatButton *b2 = new FlatButton(":/images/image_normal.png", ":/images/image_hover.png");
-    FlatButton *b3 = new FlatButton(":/images/slider_normal.png", ":/images/slider_hover.png");
+    //FlatButton *b3 = new FlatButton(":/images/slider_normal.png", ":/images/slider_hover.png");
 
     grid->addWidget(b1, 0, 0);
     grid->addWidget(b2, 0, 1);
-    grid->addWidget(b3, 0, 2);
+    //grid->addWidget(b3, 0, 2);
 
     QPushButton *closeButton = new QPushButton("Close");
 
@@ -52,7 +52,7 @@ ModulDialog::ModulDialog()
     connect(closeButton, SIGNAL(clicked(bool)), this, SLOT(close()));
     connect(b1, SIGNAL(clicked()), this, SLOT(close1()));
     connect(b2, SIGNAL(clicked()), this, SLOT(close2()));
-    connect(b3, SIGNAL(clicked()), this, SLOT(close3()));
+    //connect(b3, SIGNAL(clicked()), this, SLOT(close3()));
 }
 
 void ModulDialog::close1()
