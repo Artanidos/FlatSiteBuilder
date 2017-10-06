5262f0d14b9de1200338374cadb85040c633881a
Olaf Japp <japp.olaf@gmail.com>
2017-10-06 20:57:34 +0200

    verion no changed

--- buildAppImage.sh
+++ buildAppImage.sh
@@ -32,7 +32,7 @@ export LD_LIBRARY_PATH=~/SourceCode/FlatSiteBuilder/AppImage/usr/lib:~/Qt/5.9.1/
 rm AppImage/AppRun
 cp AppRun AppImage
 /home/olaf/appimagetool/appimagetool-x86_64.AppImage AppImage
-mv FlatSiteBuilder-x86_64.AppImage FlatSiteBuilder-Linux-x86_64-1.4.3.AppImage
+mv FlatSiteBuilder-x86_64.AppImage FlatSiteBuilder-Linux-x86_64-1.4.4.AppImage
 
 # Clean up
 rm -r AppImage/*
